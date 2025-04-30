9c044600fe723cb83a7685d9d75bad3437f4ed9b
infj1412 <leema435@naver.com>
2025-04-30 19:02:15 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    int num;
-    printf("숫자를 입력하세요: ");
-    scanf("%d", &num);
-
-    if (num % 2 == 0) {
-        printf("%d은 짝수입니다.\n", num);
-    } else {
-        printf("%d은 홀수입니다.\n", num);
+// 최대공약수(GCD)를 구하는 함수
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
     }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+    printf("두 수를 입력하세요: ");
+    scanf("%d %d", &num1, &num2);
+
+    int result = gcd(num1, num2);
+    printf("%d와 %d의 최대공약수(GCD)는: %d\n", num1, num2, result);
 
     return 0;
 }
